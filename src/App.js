c3223d09c8ad9f3240108a007dfb463c43906f86
shantiprasad1339 <118549601+shantiprasad1339@users.noreply.github.com>
2023-03-08 13:00:27 +0530

    another

--- src/App.js
+++ src/App.js
@@ -1,72 +1,111 @@
-
-
+import React from 'react';
 import { useEffect, useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
-import MovieList from './Movielistproject/MovieList';
-import Addfav from './Movielistproject/Addfav';
-import Removefav from './Movielistproject/Removefav';
+
 
 function App() {
   const [movies, setMovie] = useState([]);
-  const [favourites, setfaourites] = useState([]);
+  const [favouritMovies, setfavouritemovies] = useState([]);
   const [search, setsearch] = useState('');
+  
 
+
+  //displaying movie from api
   const getMoviesrequest = async (search) => {
-    const url = `http://www.omdbapi.com/?s=${search}&apikey=865d26de`;
+    const url = `https://www.omdbapi.com/?s=${search}&apikey=865d26de`;
     const response = await fetch(url);
     const responseJson = await response.json();
+    console.log(responseJson.Response);
+
     if (responseJson.Search) {
       setMovie(responseJson.Search);
+
     }
   };
-
   useEffect(() => {
     getMoviesrequest(search);
   }, [search]);
 
-  useEffect(() =>{
-    const moviefavourites = JSON.parse(localStorage.getItem('favouriteMovies'));
-    setfaourites(moviefavourites);
-  },[]);
-const saveToLocl = (item) => {
-  localStorage.setItem('favouriteMovies', JSON.stringify(item))
-}
+  //setting movie to localstorage
 
-  const addfavourites = (movies) => {
-    const newfavourite = [...favourites, movies]
-    setfaourites(newfavourite);
-    saveToLocl(newfavourite);
+
+
+
+
+
+  const addfavourites = (movie) => {
+    let newFavouritelist = [...favouritMovies, movie];
+    setfavouritemovies(newFavouritelist);
+    // saveTolocal(newFavouritelist)
     
   };
 
-  const Removefavmovie = (movies) => {
-    const newfavourite = favourites.filter((favourites) => favourites.imdbID !== movies.imdbID);
-    setfaourites(newfavourite);
-    saveToLocl(newfavourite);
-  }
+  const removefavourites = (movies) => {
+    const removedfavourites = favouritMovies.filter((item) => item.imdbID !== movies.imdbID);
+    setfavouritemovies(removedfavourites);
+    // localStorage.clear('movieApp');
+    // saveTolocal(removedfavourites);
+  };
+
+
+  // const saveTolocal = (moviename) => {
+  //   localStorage.setItem("movieApp", JSON.stringify(moviename));
+  // }
+  // useEffect(() => {
+  //   const getFavouritemovies = JSON.parse(localStorage.getItem('movieApp'));
+  //   setfavouritemovies(getFavouritemovies);
+    
+  // }, [search]);
   return (
     <>
-    <div className='container-fluid movieapp'>
-      <div className='row fixed-top'>
-        <div className='col'>
-          <h1 className='text-light mt-1 ms-2'>Movies</h1>
+      <div className=' h-auto w-auto'>
+        <div className='flex space-x-8 md:space-x-80 '>
+          <div className='  mt-2 bg-black rounded-lg '>
+            <h1 className='text-xl font-bold rounded-lg px-2 sm:text-3xl text-white border border-white'>Dutchman-Movies</h1>
+          </div>
+          <div className=' mt-2  '>
+            <input type='text' className='pl-2 w-40 rounded border-2 border-black ' value={search} onChange={(event) => setsearch(event.target.value)} placeholder='search movies..' ></input>
+          </div>
         </div>
-        <div className='col mt-2 '>
-          <input type='text' className='form-control w-75' value={search} onChange={(event) => setsearch(event.target.value)} placeholder='search movies..' ></input>
+        {/* <div className='max-w-screen grid grid-rows-6 grid-flow-col justify-center gap-2 sm:flex sm:space-x-2 movie-app'> */}
+        <div className=' py-5 mx-auto grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5  md:gap-x-5 xmd:grid-cols-5 gap-x-3 movie-app'>
+          {movies.map((movie, index) => {
+            return (
+              <div className=' mx-auto mb-1 flex justify-center  movie-app'>
+                <ul className='  '> <img src={movie.Poster} alt="movie" className='md:mr-40 md:w-52 rounded-xl h-72 md:h-96 border-2 border-white     transition ease-in delay-150  hover:-translate-y-1 hover:scale-110  duration-700 hover:rounded-xl' />
+
+                  <div className='-mt-9'> <i className="flex text-2xl cursor-pointer text-white fa-solid fa-heart-circle-plus justify-center hover:text-red-500 relative" onClick={() => addfavourites(movie)}></i></div>
+                  <div className='mt-[5px] flex justify-center '>
+
+                    <h1 className=' sm:text-xs md:text-sm sm:w-[180px] text-center md:mr-8 text-xs font-bold mt-1 border border-white  rounded bg-black text-white   px-2 mb-2 '>{movie.Title}</h1>
+                  </div>
+
+                </ul>
+              </div>);
+          })}
         </div>
-      </div>
-      <div className="d-flex flex-row justify-content-start mt-5 movielist">
-        <MovieList movies={movies} favouriteclick={addfavourites} favouritComponent={Addfav} />
-      </div>
-      <div className='col'>
-        <h1 className='text-light mt-1 ms-2 left-10'>Favourites</h1>
-      </div>
-    </div>
-    <div className="d-flex flex-row justify-content-start mt-5 movielist">
-        <MovieList movies={favourites} favouriteclick={Removefavmovie} favouritComponent={Removefav} />
-      </div>
-    </> 
+         <div className='  mt-2  rounded-lg flex justify-center'>
+          <h1 className='text-xl bg-black font-bold rounded-lg px-2 sm:text-3xl text-white border border-white'>Favoutites</h1>
+        </div>
+   <div className=' py-5 mx-auto grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5  md:gap-x-5 xmd:grid-cols-5 gap-x-3 movie-app'>
+          {favouritMovies.map((movie, index) => {
+            return (
+              <div className='max-w-[1200px] mx-auto mb-1 flex justify-center  movie-app'>
+                <ul className='  '> <img src={movie.Poster} alt="movie" className='sm:w-52 rounded-xl h-72 sm:h-96 border-2 border-white hover:scale-120   hover:duration-100 ' />
+
+                  <div className='-mt-9'> <i className="flex text-2xl cursor-pointer text-red-500 fa-solid fa-heart-circle-minus justify-center  " onClick={() => removefavourites(movie)}></i></div>
+                  <div className='-mt-[0px] flex justify-center  ' >
+
+                    <h1 className=' sm:text-sm w-auto text-xs font-bold mt-1 border border-white  rounded bg-black text-white   px-2 mb-2 '>{movie.Title}</h1>
+                  </div>
+
+                </ul>
+              </div>);
+          })}
+        </div>
+
+              </div>
+    </>
   );
 };
 
